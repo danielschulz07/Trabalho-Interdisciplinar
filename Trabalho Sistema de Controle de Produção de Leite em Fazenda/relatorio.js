b9afb9a39690f85cd5f87778408432a85dc836cc
Fernanda Oliveira <109592730+feroliveira59@users.noreply.github.com>
2025-07-30 20:18:43 -0300

    Add files via upload

---
+++ Trabalho Sistema de Controle de Produção de Leite em Fazenda/relatorio.js
@@ -0,0 +1,34 @@
+const outRelatorio = document.getElementById("outRelatorio");
+const btnCalcular = document.getElementById("btncalcular");
+const btnRelatorio = document.getElementById("btnRelatorio");
+var vetTotalProduzido = []; // Agora é global
+
+
+
+// N É NECESSARIO ESSA FUNCAO DE CALCULAR É SO APENAS JOGAR O CALCULO EM BAIXO DO FOR
+
+btnCalcular.addEventListener("click", calcularTotalProduzido);
+//essa parte é o calculo do total produzido de leite
+function calcularTotalProduzido() {
+    vetTotalProduzido = []; // zera e recalcula
+
+    for (let i = 0; i < vetLitrosPorDia.length; i++) {
+        let total = vetLitrosPorDia[i] * vetDiasLactacao[i];
+        vetTotalProduzido.push(total);
+    }
+}
+btnRelatorio.addEventListener("click", gerarRelatorio);
+// essa parte é para gerar relatorio de uma raça específica de maior produtor de leite
+function gerarRelatorio() {
+
+    let maior = vetTotalProduzido[0]; // E SO USAR -Number.MAX_VALIE ele vai pegar o menor numero possivel
+    let indiceMaior = 0;
+    for (let i = 1; i < vetTotalProduzido.length; i++) {
+        if (vetTotalProduzido[i] > maior) {
+            maior = vetTotalProduzido[i]; // declarar as variaveis de forma mais clara
+            indiceMaior = i;
+        }
+    }
+    outRelatorio.innerHTML = "Raça: " + vetRaca[indiceMaior] +
+        " | Total Produzido (litros): " + maior.toFixed(2) + "<br>";
+}
