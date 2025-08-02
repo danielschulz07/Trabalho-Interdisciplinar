79242107fb67b0a0414871c2f06984f10c85ebf3
danielschulz07 <danielschulzdasilva@gmail.com>
2025-08-02 16:54:40 -0300

    terminei o filtro, desisti de colocar o outSaida em formato de tabela

--- backend/filtro.js
+++ backend/filtro.js
@@ -5,13 +5,6 @@ const inVolumeMaximo = document.getElementById("inVolumeMaximo");
 const btFiltro = document.getElementById("btFiltro");
 const outSaida = document.getElementById("outSaida");
 
-const main = document.getElementById("main")
-const tabela = document.createElement("table");
-const cabecalho = document.createElement("tr");
-let titulos = ['Raça', 'Ano de Início de Lactação', 'Volume Total Produzido'];
-tabela.appendChild(cabecalho);
-
-
 btFiltro.addEventListener('click', function () {
 
     let raca = inBuscarRaca.value;
@@ -19,27 +12,6 @@ btFiltro.addEventListener('click', function () {
     const dtInicioLactacaoPesq = new Date(inAnoInicioLactacao.value + "T00:00:00");
     let volumeMinimo = Number(inVolumeMinimo.value);
     let volumeMaximo = Number(inVolumeMaximo.value);
-
-
- /*   if (document.getElementById("minhaTabela")) {
-    console.log("A tabela já está criada.");
-} else {
-    console.log("A tabela ainda não foi criada.");
-    // Aqui você pode criar a tabela, se quiser
-
-
-
-
-
-            for (let i = 0; i < titulos.length; i++) {
-    const th = document.createElement('th');
-    th.textContent = titulos[i];  //AQUI VAI PEGAR UM MINI VETOR DE TITULOS E IR COLOCANDO EM FORMA DE TH (COLUNA)
-    cabecalho.appendChild(th);  // AQUI ADICIONA ESSES TITULOS NO CORPO DA TABELA, VAZENDO ISSO N PRECISA DE COLOCAR CADA TITULO DE FORMA MANUAL E REPETITIVA
-    }
-}
-*/
-
-
 
     for (let i = 0; i < vetRaca.length; i++) {
 
@@ -111,7 +83,6 @@ btFiltro.addEventListener('click', function () {
                         (volumeMinimo < producaoTotal) &&
                         (volumeMaximo == 0 || volumeMaximo > producaoTotal)
                     ) {
-
                         flagAtendeFiltros = true;
                     }
                 } else { //não tem ano lactação, nem volume lactação está vazio
@@ -121,46 +92,9 @@ btFiltro.addEventListener('click', function () {
         }
 
         if (flagAtendeFiltros == true) {
-
-            stringDeOutPut += vetRaca[i] + "  -  " + dtInicioLactacao.toLocaleDateString() + "  -  " + producaoTotal.toFixed(0) + "<br>";
-
-            /*
-
-            if (document.getElementById("minhaTabela")) {
-                console.log("A tabela já está criada.");
-            } else {
-                console.log("A tabela ainda não foi criada.");
-                // Aqui você pode criar a tabela, se quiser
-
-
-                for (let i = 0; i < vetRaca.length; i++) { //AQUI UM FOR QUE RODA TODA ESTRUTURA DA TABELA
-                    const tr = document.createElement('tr'); // TR CRIA O CABEÇALHO
-
-                    const tdRaca = document.createElement('td');
-                    tdRaca.textContent = vetRaca[i];
-                    tr.appendChild(tdRaca);
-
-                    const tdAnoLactacao = document.createElement('td');
-                    tdAnoLactacao.textContent = dtInicioLactacao.toLocaleDateString();
-                    tr.appendChild(tdAnoLactacao);
-
-                    const tdVolumeTotalProduzido = document.createElement('td');
-                    tdVolumeTotalProduzido.textContent = producaoTotal.toFixed(1);
-                    tr.appendChild(tdVolumeTotalProduzido);
-
-
-                    tabela.appendChild(tr); // ADD O TR A TABELA
-                }
-            }
-
-
-
-
-            outSaida.appendChild(tabela) // ADD A TABELA AO MAIN NO HTML
-            */
-        }
-
-
-        outSaida.innerHTML = stringDeOutPut;
+            stringDeOutPut += "Raça: " + vetRaca[i] + "  -  " + "Começo de Lactação: " + dtInicioLactacao.toLocaleDateString() + 
+            "  -  " + "Volume de Leite Produzido: " + producaoTotal.toFixed(1) + "L" + "<br>";
+        }  
+      outSaida.innerHTML = stringDeOutPut
     }
 })
